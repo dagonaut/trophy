b88ee9b94ee7f6f08cf77caec3cfc9877c5c24e3
mx-paul <paul@mxtoolbox.com>
2025-08-18 15:33:12 -0500

    updating paths in prebuild

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-rm -rf docs
-mkdir -p docs
+rm -rf ./trophy-site/docs
+mkdir -p ./trophy-site/docs
 # EDIT this path to your Obsidian subfolder
 rsync -av --delete "./bellmead/trophy/" ./trophy-site/docs/
