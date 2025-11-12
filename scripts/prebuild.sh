613f7c0afb2a905f46b1fe448d4227c6af24a753
paulatmxtoolbox <paul@mxtoolbox.com>
2025-11-12 00:58:01 -0600

    fixing image sizing

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
+# Clear out the existing docs folder and copy over the latest from Obsidian
 rm -rf ./trophy-site/docs
 mkdir -p ./trophy-site/docs
 # EDIT this path to your Obsidian subfolder
 rsync -av --delete "./bellmead/trophy/" ./trophy-site/docs/
+
+# Convert Obsidian image resize syntax to MkDocs-friendly syntax
+find ./trophy-site/docs -type f -name '*.md' -print0 | xargs -0 perl -pi -e '
+  s/!\[\[([^|\]]+)\|(\d+)\]\]/![](\1){ width=\2 }/g
+'
